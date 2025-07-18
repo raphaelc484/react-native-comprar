4aad73646905c8031cefa1a121c7de804b14c8c8
raphaelc484 <raphaelc484@hotmail.com>
2025-07-18 14:54:02 -0300

    feat: Projeto de modelo de compra

---
+++ src/app/Home/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#d0d2d8",
+    alignItems: "center",
+    paddingTop: 62,
+    padding: 24,
+    gap: 24,
+  },
+  logo: {
+    height: 34,
+    width: 134,
+  },
+});
